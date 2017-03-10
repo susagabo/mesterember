aecee97c05c572feb6b17e3309ac22473d8bebba
susagabo <susa.gabor@gmail.com>
2017-03-10 16:57:19 +0100

    Add project files.

---
+++ feladat/Burkolo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace feladat
+{
+    public class Burkolo : Mesterember
+    {
+        private int szakterulet;
+
+        public void OsszesSzabadnap()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void MindenAdat()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void MunkatVallal()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public enum Szakterulet
+        {
+            Belso,
+            Kulso
+        }
+    }
+}
---
+++ feladat/Mesterember.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace feladat
+{
+    public class Mesterember
+    {
+        public string nev;
+        private int napidij;
+        private bool[] foglaltnapok=new bool[31];
+
+        public Mesterember(string nev, int napidij)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void MindenAdat()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void MunkatVallal()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
